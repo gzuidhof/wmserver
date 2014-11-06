b008db078572d9362ca2fca3dda9f18f0154dda2
Guido Zuidhof <me@guido.io>
2014-11-06 20:01:20 +0100

    Minimal client discovery server

--- wmserver.js
+++ wmserver.js
@@ -2,15 +2,35 @@ var express = require('express');
 var app = express();
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 
+var server = app.listen(7070);
 app.get('/', function(req, res, next) { res.send('Hello world!'); });
-
-var server = app.listen(7170);
-
 
 var options = {
     debug: true
 }
 
-app.use('/api', ExpressPeerServer(server, options));
+var pjs = ExpressPeerServer(server, options);
+
+app.use('/peerjs', pjs);
+
 
 console.log("Server started, CTRL+C to quit");
+
+var connected = [];
+
+pjs.on('connection', function (id) {
+  console.log(id + " connected.");
+  connected.push(id);
+});
+pjs.on('disconnect', function (id) {
+  console.log(id + " disconnected.");
+
+  var index = connected.indexOf(id);
+  if (index > -1) {
+    connected.splice(index, 1);
+  }
+});
+
+app.get('/connectedpeers', function (req, res) {
+  return res.json(connected);
+});
